8d88b085619037758b344ac732dd4824d95cf53c
ciscom <ciscom1971@yahoo.com>
2019-08-18 22:23:24 -0500

    Save System added. platyer controller added squirl moves to right really slow Also added swipe controls

---
+++ FreshPrints/Assets/PlayerControllerScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerScript : MonoBehaviour
+{
+    public float playerMoveSpeed = 4f;
+
+
+    private void FixedUpdate()
+    {
+        Vector3 dir = new Vector3(999, 0, 0);
+        transform.Translate(dir.normalized * playerMoveSpeed * Time.deltaTime, Space.World);
+
+    }
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    Debug.Log("collisionenter");
+
+    //    Debug.Log(gameObject.name);
+
+    //    if(collision.collider.IsTouching(collision.otherCollider))
+    //    {
+    //        Debug.Log("here");
+    //        rotateUP();
+    //    }
+
+
+    //}
+
+
+    //private void OnCollisionExit2D(Collision2D collision)
+    //{
+    //    Debug.Log("collisionExit");
+    //    rotateForward();
+    //}
+
+
+    //void rotateUP()
+    //{
+    //    Debug.Log("UP");
+    //    transform.Rotate(0, 0, 90);
+    //}
+
+    //void rotateForward()
+    //{
+    //    Debug.Log("forward");
+    //    transform.Rotate(0, 0, 0);
+    //}
+
+
+}
---
+++ FreshPrints/Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public int level = 3;
+    public int health = 100;
+
+    // will also save the player postion... tranform...j
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SavePlayer()
+    {
+        SaveSystem.SavePlayer(this);
+    }
+
+    public void LoadPlayer()
+    {
+        PlayerData data = SaveSystem.LoadPlayer();
+
+        level = data.level;
+        health = data.health;
+
+        Vector3 position = new Vector3(0,0,0);
+        position.x = data.position[0];
+        position.y = data.position[1];
+        position.x = data.position[2];
+        transform.position = position;
+
+
+    }
+
+
+
+
+
+}
