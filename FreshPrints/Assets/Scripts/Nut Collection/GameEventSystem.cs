c8a99cd1f702076e991be32cc672628591fec737
Lance Bailey <37199007+LanceBailey@users.noreply.github.com>
2019-08-21 22:15:54 -0500

    Feature/35 nut collection (#42)
    * Crude shitty import of all things in the auto run from HeartHome
    
    * Cleaned up unnecessary scrips imported for collection system
    
    * Collectables created, part of a reset system is in place but not quite finished.

---
+++ FreshPrints/Assets/Scripts/Nut Collection/GameEventSystem.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEventSystem : MonoBehaviour
+{
+    public delegate void AddScoreEventHandler(int value);
+    public static event AddScoreEventHandler ScoreAdded;
+
+    public delegate void ResetLevelEventHandler();
+    public static event ResetLevelEventHandler LevelReset;
+
+    public delegate void BeginPlayEventHandler();
+    public static event BeginPlayEventHandler BeginPlay;
+
+    public delegate void PlayerFailureEventHandler();
+    public static event BeginPlayEventHandler PlayerFail;
+
+    public delegate void EndLevelEventHandler(bool didIWin);
+    public static event EndLevelEventHandler EndLevel;
+
+
+    void Start()
+    {
+        OnLevelReset();
+    }
+
+    public static void OnScoreAdded(int score)
+    {
+        if(ScoreAdded != null)
+        {
+            ScoreAdded(score);
+        }
+    }
+
+    public static void OnLevelReset()
+    {
+        if(LevelReset != null)
+        {
+            LevelReset();
+        }
+    }
+
+    public static void OnBeginPlay()
+    {
+        if(BeginPlay != null)
+        {
+            BeginPlay();
+        }
+    }
+
+    public static void OnPlayerFail()
+    {
+        if(PlayerFail != null)
+        {
+            PlayerFail();
+        }
+    }
+
+    public static void OnEndLevel(bool didIWin)
+    {
+        if(EndLevel != null)
+        {
+            EndLevel(didIWin);
+        }
+    }
+}
---
+++ FreshPrints/Assets/Scripts/Nut Collection/JumpingGameCollectable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpingGameCollectable : MonoBehaviour {
+
+    
+    private CollectAcorn acorn;
+
+    void Start()
+    {
+        acorn = FindObjectOfType<CollectAcorn>();
+    }
+
+    public void OnCollected()
+    {
+        SpriteRenderer mySprite;
+        CircleCollider2D myCollider;
+
+        mySprite = GetComponent<SpriteRenderer>();
+        myCollider = GetComponent<CircleCollider2D>();
+
+        mySprite.enabled = false;
+        myCollider.enabled = false;
+
+        if (acorn)
+            acorn.CollectableAnimation(transform.position);
+    }
+    
+}
