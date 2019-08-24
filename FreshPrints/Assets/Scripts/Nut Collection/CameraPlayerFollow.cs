845ff938e79ef11e4b3810190d3956635b94b97a
Lance Bailey <37199007+LanceBailey@users.noreply.github.com>
2019-08-24 12:05:06 -0500

    Feature/33 Camera Controller (#44)
    * Crude shitty import of all things in the auto run from HeartHome
    
    * Cleaned up unnecessary scrips imported for collection system
    
    * Collectables created, part of a reset system is in place but not quite finished.
    
    * Camera modifications to check gameplay feel

--- FreshPrints/Assets/Scripts/Nut Collection/CameraPlayerFollow.cs
+++ FreshPrints/Assets/Scripts/Nut Collection/CameraPlayerFollow.cs
@@ -9,13 +9,14 @@ public class CameraPlayerFollow : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        playerTransform = FindObjectOfType<GameController2D>().GetComponent<Transform>();
+        playerTransform = FindObjectOfType<scr_squirrel>().GetComponent<Transform>();
 
         cameraOffset = transform.position - playerTransform.position;
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position = playerTransform.position + cameraOffset;
+        transform.position = new Vector3((playerTransform.position.x + cameraOffset.x), 0, 0);
+
 	}
 }
