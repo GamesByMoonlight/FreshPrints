5201dd1360dca2fcd1d87a870e213049247e1fd9
Lance <lance.bailey@gmail.com>
2019-10-13 15:03:03 -0500

    Created parent game object, create child boxes to have them move

---
+++ FreshPrints/Assets/terrainMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class terrainMovement : MonoBehaviour
+{
+    private IPlatformController CCC;
+    private float initialY, transformedY;
+
+    void Start() {
+
+        try
+        {
+            CCC = GetComponentInParent<IPlatformController>();
+        }
+        catch
+        {
+            Debug.LogError("Parent object of " + this + " must have IPlatformController interface");
+        }
+
+        initialY = transform.localPosition.y;
+        transformedY = -initialY;
+    }
+
+    void Update()
+    {
+        Debug.Log(CCC.PlatformPosition);
+        float interpolator = Mathf.Clamp(((CCC.PlatformPosition + 1f) / 2f), 0f, 1f);
+        transform.localPosition = new Vector3(transform.localPosition.x, Mathf.Lerp(initialY, transformedY, interpolator), transform.localPosition.z);
+    }
+
+
+}
+
+interface IPlatformController
+{
+    float PlatformPosition { get; set; }
+}
