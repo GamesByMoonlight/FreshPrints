40a8668e94bd35c6b3da3964a3bab3af3e5b6028
Lance Bailey <37199007+LanceBailey@users.noreply.github.com>
2019-10-13 19:33:24 -0500

    Created parent game object, create child boxes to have them move (#55)

---
+++ FreshPrints/Assets/terrainMovementController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class terrainMovementController : MonoBehaviour, IPlatformController
+{
+    public float PlatformPosition { get; set; }
+
+    public float min = -1.0f;
+    public float max = 1.0f;
+
+    float interpolator;
+    
+    void Start()
+    {
+        interpolator = 0f;
+    }
+
+    void Update()
+    {
+        interpolator += Time.deltaTime;
+
+        PlatformPosition = Mathf.Lerp(min, max, interpolator);
+
+        if (PlatformPosition >= max)
+        {
+            interpolator = 0;
+
+            float temp = max;
+            max = min;
+            min = temp;
+        }
+    }
+}
